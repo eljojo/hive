13d398282bb875206c5a7d096c79bcafdd8ecc73
José Albornoz <jojo@eljojo.net>
2022-11-15 14:53:26 -0500

    try to build frontend with flakes

--- flake.nix
+++ flake.nix
@@ -19,25 +19,24 @@
           config.allowUnfree = true;
         };
 
-        ripgrep = pkgs.rustPlatform.buildRustPackage rec {
-          pname = "ripgrep";
-          version = "12.1.1";
+        frontend = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "hive-frontend";
+          version = "beta";
 
-          src = pkgs.fetchFromGitHub {
-            owner = "BurntSushi";
-            repo = pname;
-            rev = version;
-            sha256 = "1hqps7l5qrjh9f914r5i6kmcz6f1yb951nv4lby0cjnp5l253kps";
+          doCheck = false;
+
+          src = builtins.path {
+            filter = path: type: type != "directory" || baseNameOf path != "archive";
+            path = ./frontend;
+            name = "src";
           };
 
           cargoSha256 = "03wf9r2csi6jpa7v5sw5lpxkrk4wfzwmzx7k3991q3bdjzcwnnwp";
 
-          meta = with flake-utils.lib; {
-            description = "A fast line-oriented regex search tool, similar to ag and ack";
-            homepage = "https://github.com/BurntSushi/ripgrep";
-            license = licenses.unlicense;
-            maintainers = [ maintainers.tailhook ];
-          };
+          # meta = with flake-utils.lib; {
+          #   description = "A fast line-oriented regex search tool, similar to ag and ack";
+          #   homepage = "https://github.com/BurntSushi/ripgrep";
+          # };
         };
 
         # ruby = pkgs.ruby_3_1;
@@ -127,13 +126,14 @@
         devShells.default = with pkgs; mkShell {
           buildInputs = [
             # rubyEnv (lowPrio rubyEnv.wrappedRuby) skopeo pkgs.chromedriver # hydrofetch
+            frontend
           ];
         };
 
         packages = {
           # ociImage = hydrofetchDockerImage;
           # default = rubyEnv;
-          default = ripgrep;
+          default = frontend;
         };
       });
 }
